cd2014116f6c277b69ecfe552da017e8d265c0e5
Chris Clarke <clarkec@sportys.com>
2024-02-05 15:37:37 -0500

    updates for vision

--- Swifty360Player/Swifty360Window.swift
+++ Swifty360Player/Swifty360Window.swift
@@ -26,13 +26,9 @@ import UIKit
 
 extension UIWindow {
     static var orientation: UIInterfaceOrientation {
-        if #available(iOS 13.0, *) {
             return UIApplication.shared.windows
                 .first?
                 .windowScene?
                 .interfaceOrientation ?? .portrait
-        } else {
-            return UIApplication.shared.statusBarOrientation
-        }
     }
 }
